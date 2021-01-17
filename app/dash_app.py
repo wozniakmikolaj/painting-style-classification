8031bb7f52cc95282695f65273d8fc98e05b8a4c
Mikolaj Wozniak <wozniak.mik@gmail.com>
2021-01-17 23:46:57 +0100

    Finished the dashboard.

--- app/dash_app.py
+++ app/dash_app.py
@@ -14,89 +14,61 @@ import dash
 from dash.dependencies import Input, Output, State
 import dash_core_components as dcc
 import dash_html_components as html
+import dash_bootstrap_components as dbc
 import plotly_express as px
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-LABELS_LIST = ['ART NOUVEAU (MODERN)', 'BAROQUE', 'CUBISM', 'EXPRESSIONISM', 'IMPRESSIONISM',
+LABELS_LIST = ['ART NOUVEAU', 'BAROQUE', 'CUBISM', 'EXPRESSIONISM', 'IMPRESSIONISM',
                'REALISM', 'ROMANTICISM', 'SURREALISM', 'SYMBOLISM', 'UKIYO-E']
 
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.FLATLY],
+                meta_tags=[{'name': 'viewport',
+                            'content': 'width=device-width, initial-scale=1.0'}]
+                )
 
 
 def serve_layout():
-    return html.Div(
-        # Basically the whole page
-        id="root",
-        children=[
-            # Main site body
-            html.Div(
-                id="app-container",
-                children=[
-                    # Main site title
-                    html.Div(
-                        id="app-title",
-                        children=[
-                            html.H2("Image Classification App",
-                                    id="title",
-                                    style={'text-align': 'center'}),
-                            html.Hr()
-                        ],
-                    ),
-                    # Image
-                    html.Div(
-                        id='output-image-upload'
-                    )
-                ]
-                # style={
-                #     'width': '70%',
-                #     'left': '175px',
-                #     'height': '100%',
-                #     'float': 'right',
-                #     'position': 'relative',
-                # }
-            ),
-            # Sidebar
-            html.Div(
-                id="sidebar",
-                children=[
-                    dcc.Upload(
-                        id='upload-image',
-                        children=html.Div([
-                            'Drag and Drop or ',
-                            html.A('Select Files')
-                        ]),
-                        style={
-                            'width': '80%',
-                            'height': '60px',
-                            'lineHeight': '60px',
-                            'borderWidth': '1px',
-                            'borderStyle': 'dashed',
-                            'borderRadius': '5px',
-                            'textAlign': 'center',
-                            'margin': '10px'
-                        },
-                        # Allow multiple files to be uploaded
-                        multiple=True
-                    ),
-                    html.Br(),
-                    html.Button('Classify painting style', id='classify_style'),
-                    html.Br(),
-                    dcc.Graph(id='probabilities-graph'),
-                    html.Br(),
-                    html.Div('Predicted class:', id='predicted_class')
-                ]
-                # style={
-                #     'width': '30%',
-                #     # 'height': '250px',
-                #     'background': 'green',
-                #     'position': 'fixed',
-                #     'height': '100%',
-                # }
-            ),
-            # hidden div to share data between callbacks in users' browser session
-            html.Div(id='intermediate-value', style={'display': 'none'})
-        ]
-    )
+    return dbc.Container([
+        dbc.Row([
+            dbc.Col([html.H2("Painting style classification", className='text-center'),
+                     html.Hr(),
+                     html.Br(),
+                     html.Br(),
+                     html.Div(id='output-image-upload'),
+                     html.Br(),
+                     html.Br(),
+                     html.Hr(),
+                     ], width={'size': 8}),
+            dbc.Col([
+                dcc.Upload(
+                    id='upload-image',
+                    children=html.Div([
+                        'Drag and Drop or ',
+                        html.A('Select Files')
+                    ]),
+                    style={
+                        'width': '100%',
+                        'height': '60px',
+                        'lineHeight': '60px',
+                        'borderWidth': '1px',
+                        'borderStyle': 'dashed',
+                        'borderRadius': '5px',
+                        'textAlign': 'center'
+                    },
+                    multiple=True
+                ),
+                html.Br(),
+                html.Button(id='classify_style', n_clicks=0,
+                            children='Classify painting style',
+                            className='btn-block'),
+                html.Br(),
+                dcc.Graph(id='probabilities-graph'),
+                html.Br(),
+                html.H6('Predicted class:', id='predicted_class', className='text-center'),
+                # hidden div to share data between callbacks in users' browser session
+                html.Div(id='intermediate-value', style={'display': 'none'})
+            ], width={'size': 4})
+        ], justify='center', style={'margin-top': '30px'})
+    ], fluid=True)
 
 
 app.layout = serve_layout
@@ -104,28 +76,13 @@ app.layout = serve_layout
 
 def display_image(content):
     return html.Div([
-        html.Img(src=content[0]),
-        html.Hr()
+        html.Img(src=content[0],
+                 style={'display': 'block',
+                        'margin-left': 'auto',
+                        'margin-right': 'auto'
+                        }
+                 )
     ])
-
-
-# A placeholder callback with img in binary, serving as an intermediate data source
-@app.callback(Output('intermediate-value', 'children'),
-              Input('upload-image', 'contents'))
-def pass_intermediate_value(contents):
-    if contents is not None:
-        return contents
-
-
-# Displays uploaded image
-@app.callback(Output('output-image-upload', 'children'),
-              Input('intermediate-value', 'children'))
-def update_img_output(contents):
-    if contents is not None:
-        children = [
-            display_image(contents)
-        ]
-        return children
 
 
 def _decode(raw_img_string):
@@ -153,11 +110,31 @@ def convert_prob_labels(list_of_probabilities, list_of_labels):
 
 def draw_probability_plot(raw_probabilities, class_list=LABELS_LIST):
     probabilities, labels = convert_prob_labels(raw_probabilities, class_list)
-    figure = px.bar(x=labels, y=probabilities, labels={'x': 'labels', 'y': 'probabilities'})
+    figure = px.bar(x=labels, y=probabilities,
+                    labels={'x': '', 'y': 'probabilities'})
     return figure
 
 
-# works again babyyyyy
+# A placeholder callback with img in binary, serving as an intermediate data source
+@app.callback(Output('intermediate-value', 'children'),
+              Input('upload-image', 'contents'))
+def pass_intermediate_value(contents):
+    if contents is not None:
+        return contents
+
+
+# Displays uploaded image
+@app.callback(Output('output-image-upload', 'children'),
+              Input('intermediate-value', 'children'))
+def update_img_output(contents):
+    if contents is not None:
+        children = [
+            display_image(contents)
+        ]
+        return children
+
+
+# Draws probability graph and
 @app.callback([Output('predicted_class', 'children'),
                Output('probabilities-graph', 'figure')],
               Input('intermediate-value', 'children'))
@@ -166,39 +143,12 @@ def perform_classification(img_data):
         decoded_image = _decode(img_data)
         class_probabilities = classify(decoded_image)
         predicted_class = np.argmax(class_probabilities)
-        success_string = f"The predicted class is: {predicted_class}"
+        success_string = f'Painting style : {LABELS_LIST[predicted_class]}'
         children = [
              html.H5(success_string)
         ]
         figure = draw_probability_plot(class_probabilities)
         return children, figure
-
-
-# this one works fine
-# @app.callback(Output('predicted_class', 'children'),
-#               Input('intermediate-value', 'children'))
-# def perform_classification(img_data):
-#     if img_data is not None:
-#         decoded_image = _decode(img_data)
-#         class_probabilities = classify(decoded_image)
-#         predicted_class = np.argmax(class_probabilities)
-#         success_string = f"The predicted class is: {predicted_class}"
-#         children = [
-#              html.H5(success_string)
-#         ]
-#         return children
-
-# @app.callback([Output('predicted_class', 'children'),
-#               Output('probabilities-graph', 'figure')],
-#               [Input('intermediate-value', 'children'),
-#               Input('classify_style', '_')])
-# def perform_classification(img_data):
-#     if img_data is not None:
-#         children = [
-#              classify_image(img_data)
-#         ]
-#         figure = px.bar()
-#         return children, figure
 
 
 if __name__ == '__main__':
